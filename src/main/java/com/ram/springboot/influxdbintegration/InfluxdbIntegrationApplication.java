0cf944e47923f41e968e493bc0609d92a2f8191a
ram <raky646@hotmail.com>
2023-07-06 10:22:42 -0400

    Initial commit

---
+++ src/main/java/com/ram/springboot/influxdbintegration/InfluxdbIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.ram.springboot.influxdbintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfluxdbIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfluxdbIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ram/springboot/influxdbintegration/InfluxdbIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ram.springboot.influxdbintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfluxdbIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
